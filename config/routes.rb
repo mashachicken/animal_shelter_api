c173a3bbfe361a62ac20fba35da9b418acf62196
Masha Ivy <mashanechmo@gmail.com>
2020-01-31 11:15:02 -0800

    start over as an API ONLY project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-RaiRails.application.routes.draw do
-  root 'places#index'
-  resources :places
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
