46d3d756c50c318d4defd2a5c4a0ed05cf769990
Masha Ivy <mashanechmo@gmail.com>
2020-01-31 16:08:05 -0800

    add routes

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,7 +4,25 @@ class AnimalsController < ApplicationController
     @animals = {"animal": "cat."}
     json_response(@animals)
   end
+  def show
+    @animal = Animal.find(params[:id])
+    json_response(@animal)
+  end
 
+  def create
+    @animal = Animal.create(quote_params)
+    json_response(@animal)
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update(quote_params)
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+  end
   private
   def json_response(object, status = :ok)
     render json: object, status: status
